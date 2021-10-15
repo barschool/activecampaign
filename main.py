c142483ebe9f286773e14623108e6793e97c2c2d
Peter Larsson <peter@barschool.net>
2021-10-15 10:14:34 +0700

    Better logging

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ from db import courses_by_startdate
 
 DATE = date.today()
 TYPES = ['basic-bartending-cou','4-week-international','advanced-bartender-c','barista-academy']
+no_results = True
 
+print(f"Finding courses...")
 for (num_bookings, start_date, course_ref, destination_ref, name,) in courses_by_startdate(startdate=DATE, course_type_refs=TYPES):
+    no_results = False
     print(f"{course_ref} in {name} starting today, finding ac contact id...")
     contact_id = find_contact_id(name)
     if contact_id:
@@ -20,3 +23,5 @@ for (num_bookings, start_date, course_ref, destination_ref, name,) in courses_by
     else:
         print(f"Contact {name} not found in ActiveCampaign.")
 
+if no_results:
+    print('No courses starting today found.')
